f0eaee1469ccf18449f56e08e69008539e74d701
Unknown <larisayablonskaya@gmail.com>
2017-04-29 18:08:54 +0300

    sandbox

--- sandbox/playground.js
+++ sandbox/playground.js
@@ -7,20 +7,20 @@ var driver = new webdriver.Builder()
     .build();
 
 
-driver	
+driver    
 .then( _ => driver.get('https://dashboard-beta.zuznow.com/user/register') )
 .then( _ => logStep('waiting for mail-account varification e-mail') )
 
 /*
 .then( _ => click('edit-mail') )
 .then( _ => click( { 
-	by: { id: 'edit-mail' },
-	timeout: 0 
+    by: { id: 'edit-mail' },
+    timeout: 0 
 } ) )
 .then( _ => act( { 
-	by: 	 { id: 'edit-mail' },
-	timeout: 0,
-	action:  'click',
+    by:      { id: 'edit-mail' },
+    timeout: 0,
+    action:  'click',
 } ) )
 
 */
@@ -30,44 +30,62 @@ driver
 
 
 .then( _ => 
-	driver.wait(until.elementLocated(By.id( 'edit-mail' ))) 
-		.then( e => driver.wait(until.elementIsVisible( e ) ) )
-	    .then( e => e.click() )
+    driver.wait(until.elementLocated(By.id( 'edit-mail' ))) 
+        .then( e => driver.wait(until.elementIsVisible( e ) ) )
+        .then( e => e.click() )
 )
 
 function logStep() {
-	let args = [].slice.apply(arguments)
-	args.unshift("    %s")
-	console.log.apply( console, args ) 
+    let args = [].slice.apply(arguments)
+    args.unshift("    %s")
+    console.log.apply( console, args ) 
 }
 
 
 var sa = require('selenium-actor');
-
-user1 = sa.chromeUser();
-
-sa.learnDsl({
-  "google search box"    : By.name("q")
-  "google search button" : By.id("btnG")
+sa.learnPages({
+  "google home page"  : {
+    url:      "https://www.google.com"
+    controls: { 
+      "search query"  : By.name("q"),
+      "search button" : By.id("btnG"),
+    }
+  },
+  "registration page" : {
+    url: "https://dashboard-beta.zuznow.com/user/register",
+    controls: { 
+      "email"         : By.id("edit-mail"),
+      "first name"    : By.id("edit-field-first-name-und-0-value"),
+      "last name"     : By.id("edit-field-last-name-und-0-value"),
+      "submit"        : By.id("edit-submit")
+    }
+  }
 })
 
+user1 = sa.chromeUser();
+//eq user = sa.user({ forBrowser: "chrome" })
+
+
+
 user1
-  .opens("http://google.com")
-  .input({ 
-	"registration name":  "test",
-	"registration mail":  "registration",
-	"registration phone": "05475263525",
-	"gender":             "M"
+  .opens("https://dashboard-beta.zuznow.com/user/register")
+  .inputs({ 
+    "registration email":       mailUser + '@mailinator.com',
+    "registration first name":  "registration",
+    "registration last name":   "test",
+    "registration mail":        "registration",
+    "registration phone":       "05475263525",
+    "gender":                   "M"
   })
-  .click("google search button")
+  .clicks("google search button")
   .waitsFor(sa.url, "search?q=selenium")
   .checks({
     "search button to be present": {
       "btnG" : e => !e.isFound()
     },
-	"page title to contain 'search results'" : {
-	  [sa.title]: title => title.contains('selenium')
-	}
+    "page title to contain 'search results'" : {
+      [sa.title]: title => title.contains('selenium')
+    }
   })
  
 
