371ee4c64c53bb08d8a8d300d7f4e54bc94c0895
Unknown <larisayablonskaya@gmail.com>
2017-04-30 11:30:44 +0300

    New LogIn scenario

--- plain-selenium/log-in.js
+++ plain-selenium/log-in.js
@@ -12,4 +12,14 @@ driver
 .catch( z.failedScenario )
 
 driver
+.then( _ => z.scenario('Log in with wrong password'))
+.then( _ => z.openPage ('https://dashboard-beta.zuznow.com/user/login', 'login page') )
+.then( _ => z.inputById('edit-name','Tom@zuznow.com'))
+.then(  _ => z.inputById('edit-pass', 'Computer1'))
+.then( _ => z.clickById('edit-submit') )
+.then( _ => z.assertExists(By.linkText('Have you forgotten your password?'), "recover password link" ) )
+.catch( z.failedScenario )
+
+ 
+driver
 .then( _ => z.endResult() )
