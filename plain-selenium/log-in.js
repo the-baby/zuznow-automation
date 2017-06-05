7868ed1b2def6ba8cc59d1c1e841fc5b150247fc
Unknown <larisayablonskaya@gmail.com>
2017-06-05 15:18:23 +0300

    changes for conversation.one

--- plain-selenium/log-in.js
+++ plain-selenium/log-in.js
@@ -5,34 +5,37 @@ const driver = z.getDriver();
 //Log In functionality testing with an admin user
 driver
 .then( _ => z.scenario('Log In without password should show an error message') )
-.then( _ => z.openPage('https://dashboard-beta.zuznow.com/user/login', 'login page') )
+.then( _ => z.openPage('https://dashboard-beta.conversation.one/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Tom@zuznow.com') )
 .then( _ => z.clickById('edit-submit') )
+.then( _ => z.waitFor(3))
 .then( _ => z.assertExists(By.linkText('Have you forgotten your password?'), "recover password link" ) )
 .catch( z.failedScenario )
 
 driver
 .then( _ => z.scenario('Log in with wrong password should show an error message'))
-.then( _ => z.openPage ('https://dashboard-beta.zuznow.com/user/login', 'login page') )
+.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Tom@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Computer1'))
 .then( _ => z.clickById('edit-submit') )
+.then( _ => z.waitFor(3))
 .then( _ => z.assertExists(By.linkText('Have you forgotten your password?'), "recover password link" ) )
 .catch( z.failedScenario )
 
 driver
 .then( _ => z.scenario('Log in with wrong username should show an error message'))
-.then( _ => z.openPage ('https://dashboard-beta.zuznow.com/user/login', 'login page') )
+.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Tim@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Adminuser1'))
 .then( _ => z.clickById('edit-submit') )
+.then( _ => z.waitFor(3))
 .then( _ => z.assertExists(By.linkText('Have you forgotten your password?'), "recover password link" ) )
 .catch( z.failedScenario )
 
 driver
-.then( _ => z.scenario('successful log in of user with applications should show homepage'))
+.then( _ => z.scenario('successful log in of admin user with applications should show homepage'))
 .then( _ => z.clearCookies() )
-.then( _ => z.openPage ('https://dashboard-beta.zuznow.com/user/login', 'login page') )
+.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Tom@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Adminuser1'))
 .then( _ => z.clickById('edit-submit') )
@@ -42,17 +45,17 @@ driver
 driver
 .then( _ => z.scenario('successful log in of users without applications should show new-app screen'))
 .then( _ => z.clearCookies() )
-.then( _ => z.openPage ('https://dashboard-beta.zuznow.com/user/login', 'login page') )
+.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Polly@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Newuser1'))
 .then( _ => z.clickById('edit-submit') )
-.then( _ => z.assertExists(By.id('domain'), "enter domain field" ) )
+.then( _ => z.assertExists(By.id('industry'), "select industry" ) )
 .catch( z.failedScenario )
 
 driver
 .then( _ => z.scenario('successful log in of non-admin user with applications should show homepage'))
 .then( _ => z.clearCookies() )
-.then( _ => z.openPage ('https://dashboard-beta.zuznow.com/user/login', 'login page') )
+.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Martin@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Newuser2'))
 .then( _ => z.clickById('edit-submit') )
