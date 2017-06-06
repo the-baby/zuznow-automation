9bab4d0cceac0f275fa41836757c40cfa7ca0001
Unknown <larisayablonskaya@gmail.com>
2017-06-06 16:00:40 +0300

    new mail

--- plain-selenium/registration.js
+++ plain-selenium/registration.js
@@ -65,9 +65,9 @@ driver
 driver
 .then( _ => scenario('submission of a mail in use should offer the recover password message') )
 .then( _ => openPage('https://dashboard-beta.conversation.one/user/register', 'register page') )
-.then( _ => inputById('edit-mail','larisa@zuznow.com') )
-.then( _ => inputById('edit-field-first-name-und-0-value','Larisa') )
-.then( _ => inputById('edit-field-last-name-und-0-value','El-Netanany') )
+.then( _ => inputById('edit-mail','Tom@zuznow.com') )
+.then( _ => inputById('edit-field-first-name-und-0-value','Name') )
+.then( _ => inputById('edit-field-last-name-und-0-value','LastName') )
 .then( _ => clickById('edit-submit') )
 .then( _ => waitFor(3))
 .then( _ => clickByLinkText('Have you forgotten your password?') )
@@ -75,24 +75,28 @@ driver
 .catch( failedScenario )
 
 //submission of valid form details should succeed
-var mailUser = "reg-tst-" + Math.random().toString().replace("0.","").substr(0,25)
-driver	
+
 .then( _ => scenario('submission of valid form details should succeed') )
 .then( _ => openPage('https://dashboard-beta.conversation.one/user/register', 'register page') )
-.then( _ => inputById('edit-mail', mailUser + '@mailinator.com' ) )
+.then( _ => inputById('edit-mail', 'automationlarisa@gmail.com' ) )
 .then( _ => inputById('edit-field-first-name-und-0-value','registration') )
 .then( _ => inputById('edit-field-last-name-und-0-value','test') )
 .then( _ => clickById('edit-submit') )
-.then( _ => openPage('https://www.mailinator.com/inbox2.jsp?public_to=' + mailUser + '#/#public_maildirdiv', 'inbox of: ' + mailUser.cyan) )
-.then( _ => clickByClassName('innermail ng-binding') )
+.then( _ => openPage('https://mail.google.com/mail/u/1/#inbox' ) )
+.then( _ => inputById('identifierId', 'automationlarisa') )
+.then( _ => waitFor(3))
+.then ( _ => clickByClassName('CwaK9'))
+.then( _ => inputByName('password', 'Apples32') )
+.then( _ => waitFor(3))
+.then ( _ => clickByClassName('CwaK9'))
 .then( _ => 
 	logStep('focusing on mail body IFrame')  
- || locate(By.id('publicshowmaildivcontent') )
+ || locate(By.className('info@conversation.one') )
      .then( e => substep("switching") || driver.switchTo().frame(e) )
 )
 .then( _ => 
-	logStep("navigating to URL from mail ", "/html/body/a[1]".magenta)
- || locate(By.xpath("/html/body/a[1]"))
+	logStep("navigating to URL from mail ", "/html/body/a[2]".magenta)
+ || locate(By.xpath("/html/body/a[2]"))
 	 .then( e => 
 		 substep("navigatin to href") 
 	  || driver.get( e.getAttribute("href") ) 
