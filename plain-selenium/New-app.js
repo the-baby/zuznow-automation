8097e0e151d55d280ca2d6df90cefade21a0f0a6
Unknown <larisayablonskaya@gmail.com>
2017-06-17 17:27:42 +0300

    two scenarios fixed

--- plain-selenium/New-app.js
+++ plain-selenium/New-app.js
@@ -1,6 +1,7 @@
 const { By } = require('selenium-webdriver');
 const z = require('./common');
 const driver = z.getDriver();
+process.on('exit', () => z.endResult() )
 
 //Feature: Creating a new app with a custom industry
 
@@ -13,10 +14,10 @@ driver
 .then( _ => z.assertExistsByClassName('menu-item new-site'), "home page"  )
 .catch( z.failedScenario )
 
-
+/
 
 //Successful creation of a default custom app with demo content
-
+driver
 .then( _ => z.scenario('Clicking the new app button opens the Create new app screen') )
 .then( _ => z.openPage('https://dashboard-beta.conversation.one/userpage'))
 .then( _ => z.clickByClassName('menu-item new-site'))
@@ -35,7 +36,7 @@ driver
 .then( _ => z.assertExistsById('btnFinish'), "Alexa skill is ready"  )
 .catch( z.failedScenario )
 
-/*
+driver
 .then( _ => z.scenario('Checking that demo content is present in the custom app') )
 .then( _ => z.openPage(' https://dashboard-beta.conversation.one/new')) 
 .then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
@@ -47,23 +48,35 @@ driver
 .then( _ => z.waitFor(3))
 .then( _ => z.assertExistsById('btnFinish'), "Alexa skill is ready"  )
 .then( _ => z.clickByXPath ('//*[@id="btnFinish"]/span'))
-//.then( _ => z.assertExistsById('collapseCON1Launch'), 'intent from the demo content')
-//TO DO: find a way to locate intent
+.then( _ => z.locate(By.id('btnSave')))
+.then( _ => z.assertExistsByLinkText('CalcIntent'), 'intent from the demo content')
 .catch( z.failedScenario )
 */
-
 //The next button is unavailable when one of the parameters is missing
-/*
+driver
 .then( _ => z.scenario('The user remains on the same screen after clicking the Next button') )
 .then( _ => z.openPage(' https://dashboard-beta.conversation.one/new'))
 .then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
 .then( _ => z.clickByLinkText('Custom'))
-.then( _ => z.assertExistsById('btnNext'))
+.then( _ => z.assertElementHasClass(By.id('btnNext'), "`next` button is disabled", "disabled"))
+.then( _ => {
 
-//TO DO : find a way to deal with a disabled button
-*/
+  z.logStep("assertion:".yellow + "`next` button is disabled" , 'button should have class `disabled`'.magenta);
+  driver.findElement(By.id('btnNext'))
+  .getAttribute('class')
+  .then( cssclass => {
+      if(cssclass.indexOf('disabled') == -1) 
+          return Promise.reject(new Error('element does not have class `disabled`'));
+      
+      z.logStep(" - OK!".green)
+      return Promise.resolve()
+  })
+})
+.catch( z.failedScenario )
+return
 
 //Adding other voice assistants to the app
+driver
 .then( _ => z.scenario('Checking that it is possible to add other assistants to the app') )
 .then( _ => z.openPage(' https://dashboard-beta.conversation.one/new')) 
 .then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
@@ -76,6 +89,7 @@ driver
 .catch( z.failedScenario )
 
 //Customization of a new custom app with demo content
+driver
 .then( _ => z.scenario('Clicking the Customize button opens the Editor Interaction tab') )
 .then( _ => z.openPage(' https://dashboard-beta.conversation.one/new')) 
 .then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
@@ -91,6 +105,7 @@ driver
 .catch( z.failedScenario )
 
 //Successful creation of a custom app without demo content 
+driver
 .then( _ => z.scenario('Clicking the Customize button opens the Editor Interaction tab') )
 .then( _ => z.openPage(' https://dashboard-beta.conversation.one/new')) 
 .then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
@@ -110,7 +125,7 @@ driver
 //Feature: Creating a new app with a predefined bank industry
 
 //Successful creation of a predefined banking app
-
+driver
 .then( _ => z.scenario('Clicking the Next button opens the Test your skill screen') )
 .then( _ => z.openPage(' https://dashboard-beta.conversation.one/new')) 
 .then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
@@ -129,6 +144,7 @@ driver
 .catch( z.failedScenario )
 
 //Publishing of a new predefined banking app
+driver
 .then( _ => z.scenario('Clicking the Publish button opens the Publish Skill Wizard') )
 .then( _ => z.openPage(' https://dashboard-beta.conversation.one/new')) 
 .then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
@@ -137,17 +153,8 @@ driver
 .then( _ => z.clickByClassName ('pager wizard'))
 .then( _ => z.waitFor(2))
 .then( _ => z. clickById ('btnNext'))
-
 .then( _ => z.clickByClassName ('btn btn-success btn-square btn-create finish'))
 .then( _ => z.waitFor(8))
 .then( _ => z.assertExistsByLinkText('developer.amazon.com'), "the wizard is open")
 .catch( z.failedScenario )
 
-driver
-.then( _ => z.endResult() )
-
-
-
-
-
- 
