63ed1753c273f5c86e683dccb790ff1cf03502ee
Unknown <larisayablonskaya@gmail.com>
2017-07-04 16:27:17 +0300

    new scenarios added

--- plain-selenium/interaction-editor.js
+++ plain-selenium/interaction-editor.js
@@ -1,147 +1,414 @@
-const { By } = require('selenium-webdriver');
-const z = require('./common');
-const driver = z.getDriver();
-
-
-//Feature: Creating a new intent and editing its name
-
-driver
-.then( _ => z.scenario('Sign-in successfully leads to homepage') )
-.then( _ => z.openPage('https://dashboard-beta.conversation.one/user/login', 'login page') )
-.then( _ => z.inputById('edit-name','admin') )
-.then( _ => z.inputById('edit-pass','vs8Sr7aU') )
-.then( _ => z.clickById('edit-submit') )
-.then( _ => z.waitFor(3))
-.then( _ => z.assertExistsByClassName('menu-item new-site'), "home page"  )
-.catch( z.failedScenario )
-
-//Interaction Editor opens after creating a new app
-driver
-.then( _ => z.scenario('Clicking the Next button opens the Test your skill screen') )
-.then( _ => z.openPage(' https://dashboard-beta.conversation.one/new')) 
-.then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
-.then( _ => z.clickByLinkText('Banking'))
-.then( _ => z.inputById ('org-name', 'TJX Rewards'))
-.then( _ => z.clickByClassName ('pager wizard'))
-.then( _ => z.waitFor(2))
-.then( _ => z. clickById ('btnNext'))
-.then( _ => z.assertExistsById('simulator'),'the Play simulator button' )
-.catch( z.failedScenario )
-
-.then( _ => z.scenario('Clicking the Customize button opens the Interaction Editor tab') )
-.then( _ => z.clickByXPath ('//*[@id="btnFinish"]/span'))
-.then( _ => z.locate(By.id('btnSave')))
-.then( _ => z.waitFor(2))
-.then( _ => z.assertExistsById('accordion_interaction'), "the Interaction tab is open")
-.catch( z.failedScenario )
-
-//Creating an empty intent
-driver
-.then( _ => z.scenario('Creating an empty intent returns an error message') )
-.then( _ => z.clickByClassName ('input-group-addon btn'))
-.then( _ => z.assertExistsByClassName('jGrowl-notification'), 'error message')
-.catch( z.failedScenario )
-
-//Creating a new intent
-driver
-.then( _ => z.scenario('Clicking the Add button adds a new intent') )
-.then( _ => z.inputById ('newIntentInput', 'test'))
-.then( _ => z.clickByClassName ('input-group-addon btn'))
-.then( _ => z.assertExistsByLinkText('test'), 'the new intent')
-.catch( z.failedScenario )
-
-//Editing Intent name
-driver
-.then( _ => z.scenario('Clicking the Edit name button allows editing intent name') )
-.then( _ => z.waitFor(3))
-.then( _ => z.clickByXPath ('//*[@id="accordion_interaction"]/div[1]/div[1]/i[1]'))
-.then( _ => driver.findElement(By.className('newIntentName')).clear())
-.then( _ => z.inputByClassName('newIntentName', 'exam'))
-.then( _ => z.waitFor(2))
-.then( _ => z.clickByClassName ('fa fa-check'))
-.then( _ => z.assertExistsByLinkText('exam'),'intent name modified' )                        
-.catch( z.failedScenario )
-
-//TODO: add this function to common
-
-
-//Cancelling the Editing of Intent name
-driver
-.then( _ => z.scenario('Clicking the Cancel button cancels editing of intent name') )
-.then( _ => z.waitFor(3))
-.then( _ => z.clickByXPath ('//*[@id="accordion_interaction"]/div[1]/div[1]/i[1]'))
-.then( _ => driver.findElement(By.className('newIntentName')).clear())
-.then( _ => z.inputByClassName('newIntentName', 'flower'))
-.then( _ => z.waitFor(3))
-.then( _ => z.clickByXPath ('//*[@id="accordion_interaction"]/div[1]/div[1]/i[2]'))
-.then( _ => z.assertExistsByLinkText('exam'),'intent name not modified' )                        
-.catch( z.failedScenario )
-
-
-
-
-//Feature: Adding, editing and deleting a sample sentence
-
-//Successful adding of a sample sentence
-driver
-.then( _ => z.scenario('Clicking the Add button adds a new sample phrase') )
-.then( _ => z.inputByXPath ('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/div[1]/input', 'how was the test'))
-.then( _ => z.clickByXPath ('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/div[1]/i'))
-.then( _ => z.assertExistsByXPath('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/div[1]/input'), 'the new sample phrase')
-.catch( z.failedScenario )
-
-//Unsuccessful adding of a sample sentence
-driver
-.then( _ => z.scenario('Entering a phrase with invalid characters returns an error message') )
-.then( _ => z.inputByXPath ('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/div[1]/input', 'how was the test?'))
-.then( _ => z.clickByXPath ('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/div[1]/i'))
-.then( _ => z.assertExistsByClassName('jGrowl-notification'), 'error message appeared')
-.catch( z.failedScenario )
-
-//Edit sample sentence
-driver
-.then( _ => z.scenario('Clicking the edit button allows editing a sample sentence') )
-.then( _ => z.clickByCss('#collapsetest .fa-edit'))
-.then( _ => z.assertExistsByCss('.newSample'), 'the field became editable')
-.catch( z.failedScenario )
-
-/*
-//Cancel editing of a sample sentence
-driver
-.then( _ => z.scenario('Clicking cancel editing button cancels editing of a sample sentence') )
-.then( _ => z.clickByXPath('//*[@id="editbox_interaction_gui"]/div[3]/div[1]/div[2]/div/div/div/div[1]/table/tbody/tr/td[2]/a[1]/i'))
-.then( _ => driver.findElement(By.xpath('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/table/tbody/tr/td[1]/span[2]/input')).clear())
-.then( _ => z.inputByXPath ('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/table/tbody/tr/td[1]/span[2]/input', 'Was the test hard'))
-.then( _ => z.clickByXPath('//*[@id="editbox_interaction_gui"]/div[3]/div[1]/div[2]/div/div/div/div[1]/table/tbody/tr/td[1]/span[2]/i[2]'))
-.then( _ => z.assertExistsByXPath('//*[@id="editbox_interaction_gui"]/div[3]/div[1]/div[2]/div/div/div/div[1]/table/tbody/tr/td[1]/span'), 'the phrase remained the same')
-.catch( z.failedScenario )
-//TODO: how to check that the text remained the same
-
-*/
-
-//Delete sample sentence
-/*
-driver
-.then( _ => z.scenario('Clicking delete button deletes a sample sentence') )
-.then( _ => z.clickByXPath('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/table/tbody/tr/td[2]/a[2]/i'))
-.then( _ => z.assertExistsByXPath('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/div[1]/input'), 'the intent was deleted')
-*/
-//TODO:how to check that the intent was deleted
-
-/*
-//Adding discovery suggestion
-driver
-.then( _ => z.scenario('Clicking the Add button adds a new discovery suggestion') )
-.then( _ => z.inputByXPath ('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[1]/div[2]/input', 'what is my balance'))
-.then( _ => z.clickById ('btnSave'))
-.then( _ => z.assertExistsByXPath('//*[@id="editbox_interaction_gui"]/div[3]/div[1]/div[2]/div/div/div/div[1]/div[2]/input'), 'new discovery')
-.catch( z.failedScenario )
-//TODO: how to check that new text was added
-*/
-
-//Feature: Adding and deleting entities
-driver
-.then( _ => z.scenario('Clicking the add entity button allows edding a new entity') )
-.then( _ => z.clickByXPath('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[2]/div[1]/input', 'class'))
-.then( _ => z.clickByXPath('//*[@id="accordion_interaction"]/div[1]/div[2]/div/div/div/div[2]/div[1]/i'))
-
+const { By } = require('selenium-webdriver');
+
+const z = require('./common');
+
+const driver = z.getDriver();
+
+
+
+
+
+//Feature: Creating a new intent and editing its name
+
+
+
+driver
+
+.then( _ => z.scenario('Sign-in successfully leads to homepage') )
+
+.then( _ => z.openPage('https://dashboard-beta.conversation.one/user/login', 'login page') )
+
+.then( _ => z.inputById('edit-name','admin') )
+
+.then( _ => z.inputById('edit-pass','vs8Sr7aU') )
+
+.then( _ => z.clickById('edit-submit') )
+
+.then( _ => z.waitFor(3))
+
+.then( _ => z.assertExistsByClassName('menu-item new-site'), "home page"  )
+
+.catch( z.failedScenario )
+
+
+
+//Interaction Editor opens after creating a new app
+
+driver
+
+.then( _ => z.scenario('Clicking the Next button opens the Test your skill screen') )
+
+.then( _ => z.openPage(' https://dashboard-beta.conversation.one/new')) 
+
+.then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
+
+.then( _ => z.clickByLinkText('Banking'))
+
+.then( _ => z.inputById ('org-name', 'TJX Rewards'))
+
+.then( _ => z.clickByClassName ('pager wizard'))
+
+.then( _ => z.waitFor(2))
+
+.then( _ => z. clickById ('btnNext'))
+
+.then( _ => z.assertExistsById('simulator'),'the Play simulator button' )
+
+.catch( z.failedScenario )
+
+
+
+.then( _ => z.scenario('Clicking the Customize button opens the Interaction Editor tab') )
+
+.then( _ => z.clickByXPath ('//*[@id="btnFinish"]/span'))
+
+.then( _ => z.locate(By.id('btnSave')))
+
+.then( _ => z.waitFor(2))
+
+.then( _ => z.assertExistsById('accordion_interaction'), "the Interaction tab is open")
+
+.catch( z.failedScenario )
+
+
+
+//Creating an empty intent
+
+driver
+
+.then( _ => z.scenario('Creating an empty intent returns an error message') )
+
+.then( _ => z.clickByClassName ('input-group-addon btn'))
+
+.then( _ => z.assertExistsByClassName('jGrowl-notification'), 'error message')
+
+.catch( z.failedScenario )
+
+
+
+//Creating a new intent
+
+driver
+
+.then( _ => z.scenario('Clicking the Add button adds a new intent') )
+
+.then( _ => z.inputById ('newIntentInput', 'test'))
+
+.then( _ => z.clickByClassName ('input-group-addon btn'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.clickByClassName ('fa fa-refresh'))
+
+.then( _ => z.assertExistsByLinkText('test'), 'the new intent')
+
+.catch( z.failedScenario )
+
+
+
+//Editing Intent name
+
+driver
+
+.then( _ => z.scenario('Clicking the Edit name button allows editing intent name') )
+
+.then( _ => z.waitFor(3))
+
+.then( _ => z.clickByXPath ('//*[@id="accordion_interaction"]/div[1]/div[1]/i[1]'))
+
+.then( _ => driver.findElement(By.className('newIntentName')).clear())
+
+.then( _ => z.inputByClassName('newIntentName', 'exam'))
+
+.then( _ => z.waitFor(2))
+
+.then( _ => z.clickByClassName ('fa fa-check'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.clickByClassName ('fa fa-refresh'))
+
+.then( _ => z.assertExistsByLinkText('exam'),'intent name modified' )                        
+
+.catch( z.failedScenario )
+
+
+
+//TODO: add this function to common
+
+
+/*
+//Cancelling the Editing of Intent name
+
+driver
+
+.then( _ => z.scenario('Clicking the Cancel button cancels editing of intent name') )
+
+.then( _ => z.waitFor(3))
+	
+.then( _ => z.clickByCss('#accordion_interaction > div:nth-child(1) > div.panel-heading > i.fa.fa-pencil'))
+
+.then( _ => driver.findElement(By.className('intent_name')).clear())
+
+.then( _ => z.inputByClassName('newIntentName', 'flower'))
+
+.then( _ => z.waitFor(3))
+
+.then( _ => z.clickByCss ('#accordion_interaction > div:nth-child(1)  i.fa.fa-times'))
+
+.then( _ => z.waitFor(3))
+	
+.then( _ => z.assertExistsByLinkText('exam'),'intent name not modified' )                        
+
+.catch( z.failedScenario )
+
+*/
+
+//Feature: Disabling and deleting intent
+
+//Disabling intent
+
+driver
+
+.then( _ => z.scenario('Clicking the Disable button disables intent') )
+
+.then( _ => z.openPage('https://dashboard-beta.conversation.one/editor'))
+
+.then( _ => z.clickByCss ('#accordion_interaction > div:nth-child(1) > div.panel-heading > a.intentDisable.fa.fa-toggle-on.pull-right'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.clickByClassName ('fa fa-refresh'))
+
+.then( _ => z.assertExistsByCss(' #accordion_interaction > div:nth-child(1) > div.panel-heading > span'),'intent name not modified' )              
+
+.catch( z.failedScenario )
+
+//Deleting intent
+//TODO: find out how to delete an intent
+
+
+//Feature: Adding, editing and deleting a sample sentence
+
+
+
+//Successful adding of a sample sentence
+
+driver
+
+.then( _ => z.scenario('Clicking the Add button adds a new sample phrase') )
+
+.then( _ => z.openPage('https://dashboard-beta.conversation.one/editor'))
+
+.then( _ => z.inputById ('newIntentInput', 'sample'))
+
+.then( _ => z.clickByClassName ('input-group-addon btn'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.waitFor(3))
+
+.then( _ => z.inputByCss ('#collapsesample > div > div > div > div.col-md-4.samples_div > div.input-icon.right > input', 'new sample sentence'))
+
+.then( _ => z.clickByCss ('#collapsesample > div > div > div > div.col-md-4.samples_div > div.input-icon.right > i'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.waitFor(3))
+
+.then( _ => z.clickByClassName ('fa fa-refresh'))
+
+.then( _ => z.assertExistsByCss('#collapsesample td.sample_content'), 'the new sample phrase')
+
+.catch( z.failedScenario )
+
+
+
+
+//Unsuccessful adding of a sample sentence
+
+driver
+
+.then( _ => z.scenario('Clicking the Add button adds a new sample phrase') )
+
+.then( _ => z.openPage('https://dashboard-beta.conversation.one/editor'))
+
+.then( _ => z.inputById ('newIntentInput', 'larisa'))
+
+.then( _ => z.clickByClassName ('input-group-addon btn'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.waitFor(3))
+
+.then( _ => z.scenario('Entering a phrase with invalid characters returns an error message') )
+
+.then( _ => z.inputByCss ('#collapselarisa> div > div > div > div.col-md-4.samples_div > div.input-icon.right > input', 'new sample sentence?'))
+
+.then( _ => z.clickByCss ('#collapselarisa > div > div > div > div.col-md-4.samples_div > div.input-icon.right > i'))
+
+.then( _ => z.assertExistsByClassName('jGrowl-notification'), 'error message appeared')
+
+.catch( z.failedScenario )
+
+
+
+/*
+
+//Edit sample sentence
+
+driver
+
+.then( _ => z.scenario('Clicking the edit button allows editing a sample sentence') )
+
+.then( _ => z.inputByCss ('#collapselarisa > div > div > div > div.col-md-4.samples_div > div.input-icon.right > input', 'new sample sentence'))
+
+.then( _ => z.clickByCss ('#collapselarisa > div > div > div > div.col-md-4.samples_div > div.input-icon.right > i'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.waitFor(3))
+
+.then( _ => z.clickByCss('#collapselarisa > div > div > div > div.col-md-4.samples_div > table > tbody > tr > td:nth-child(2) > a.rename > i'))
+
+.then( _ => z.assertExistsByCss('#collapselarisa > div > div > div > div.col-md-4.samples_div > table > tbody > tr > td.sample_content > span.renameSpan > input'), 'the field became editable')
+
+.catch( z.failedScenario )
+
+*/
+
+
+//Delete sample sentence
+
+
+driver
+
+.then( _ => z.scenario('Clicking delete button deletes a sample sentence') )
+
+.then( _ => z.inputById ('newIntentInput', 'delete'))
+
+.then( _ => z.clickByClassName ('input-group-addon btn'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.waitFor(3))
+
+.then( _ => z.clickByCss('#accordion_interaction > div:nth-child(1) > div.panel-heading > a.deleteIntentButton > i'))
+
+.then( _ => z.assertExistsByCss('#deleteIntentModal > div > div > div.modal-body'), 'the confirmation message appeared')
+
+ .then( _ => z.clickByClassName(' btn btn-danger'))             
+
+ .catch( z.failedScenario )
+
+//TODO:how to check that the sample sentence was deleted
+
+
+
+
+//Feature: Adding and deleting entities
+
+//Successful adding of an entity
+
+driver
+
+.then( _ => z.scenario('Clicking the add entity button allows adding a new entity') )
+
+.then( _ => z.openPage('https://dashboard-beta.conversation.one/editor'))
+
+.then( _ => z.inputById ('newIntentInput', 'NewEntity'))
+
+.then( _ => z.clickByClassName ('input-group-addon btn'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.waitFor(3))
+
+.then( _ => z.inputByCss('#collapseNewEntity > div > div > div > div.col-md-4.entities_div.param_div > div.input-icon.right > input', 'class'))
+
+.then( _ => z.clickByCss('#collapseNewEntity > div > div > div > div.col-md-4.entities_div.param_div > div.input-icon.right > i'))
+
+.then( _ => z.waitFor(2))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.waitFor(2))
+	
+.then( _ => z.clickByClassName ('fa fa-refresh'))
+
+.then( _ => z.assertExistsByCss('#collapseNewEntity > div > div > div > div.col-md-4.entities_div.param_div > table.table.table-striped.table-bordered.table-hover.entities-table > tbody > tr > td.entity_name > input'), 'the new entity was added')
+
+ .catch( z.failedScenario )
+ 
+ 
+
+//adding of an empty entity
+
+driver
+
+.then( _ => z.scenario('Creating an empty entity should return an error message') )
+
+.then( _ => z.openPage('https://dashboard-beta.conversation.one/editor'))
+
+.then( _ => z.clickByCss('#collapseNewEntity > div > div > div > div.col-md-4.entities_div.param_div > div.input-icon.right > i'))
+
+.then( _ => z.assertExistsById('jGrowl'), 'error message appeared')
+
+ .catch( z.failedScenario )
+ 
+
+//Deleting entity
+
+driver
+
+
+.then( _ => z.scenario('Clicking delete button deletes an entity') )
+
+.then( _ => z.openPage('https://dashboard-beta.conversation.one/editor'))
+
+.then( _ => z.inputByCss('#collapseNewEntity > div > div > div > div.col-md-4.entities_div.param_div > div.input-icon.right > input', 'DeleteEntity'))
+
+.then( _ => z.clickByCss('#collapseNewEntity > div > div > div > div.col-md-4.entities_div.param_div > div.input-icon.right > i'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.clickByCss('#collapseNewEntity > div > div > div > div.col-md-4.entities_div.param_div > table.table.table-striped.table-bordered.table-hover.entities-table > tbody > tr > td:nth-child(3) > a > i'))
+
+.then( _ => z.assertExistsByCss('#collapseNewEntity > div > div > div > div.col-md-4.entities_div.param_div > table.table.table-striped.table-bordered.table-hover.entities-table'))
+
+ .catch( z.failedScenario )
+ 
+
+//Feature: Manage entities section
+
+//The section contanins the list of entities
+
+.then( _ => z.scenario('Clicking “Manage entities” button opens the corresponding section') )
+
+.then( _ => z.openPage('https://dashboard-beta.conversation.one/editor'))
+
+.then( _ => z.clickByCss('#editbox_interaction_gui > div.panel.panel-grey.manageEntities > div.panel-heading > a > i'))
+
+.then( _ => z.assertExistsByCss('#collapseEntities > div > form > div > div > div.col-md-4.entityTypeDiv > table > tbody > tr:nth-child(2) > td.entityName'))
+
+.catch( z.failedScenario )
+
+
+//Successful adding of a new entity
+
+.then( _ => z.scenario('Clicking Create button creates a new entity') )
+
+.then( _ => z.inputByCss('#collapseEntities > div > form > div > div > div.col-md-4.entityTypeDiv > div > input'), 'zuznow')
+
+.then( _ => z.clickByCss('#collapseEntities > div > form > div > div > div.col-md-4.entityTypeDiv > div > i'))
+
+.then( _ => z.clickById ('btnSave'))
+
+.then( _ => z.clickByClassName ('fa fa-refresh'))
+
+.then( _ => z.assertExistsByCss('#collapseEntities > div > form > div > div > div.col-md-4.entityTypeDiv > table > tbody > tr:nth-child(5) > td.entityName'))
+
+.catch( z.failedScenario )
+
+
+
