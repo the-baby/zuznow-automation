b15011688b396826f91bbc6035c274c4f536e76f
Unknown <larisayablonskaya@gmail.com>
2017-08-24 15:53:48 +0300

    changed urls

--- plain-selenium/rules-sets.js
+++ plain-selenium/rules-sets.js
@@ -4,10 +4,14 @@ const z = require('./common');
 const scenario = z.scenario;
 const driver = z.getDriver();
 
+const config = require('config')
+const baseUrl = config.baseUrl
+const admin = config.creds.admin
+
 driver
 .then( _ => z.scenario('Sign-in successfully leads to homepage') )
-.then( _ => z.openPage('https://dashboard-beta.conversation.one/user/login', 'login page') )
-.then( _ => driver.manage().window().maximize())
+.then( _ => z.openPage(baseUrl + '/user/login', 'login page') )
+.then( _ => z.maximizeWindow() )
 .then( _ => z.inputById('edit-name','admin') )
 .then( _ => z.inputById('edit-pass','vs8Sr7aU') )
 .then( _ => z.clickById('edit-submit') )
@@ -19,15 +23,15 @@ driver
 
 driver
 .then( _ => z.scenario('Clicking the new app button opens the Create new app screen') )
-.then( _ => z.openPage('https://dashboard-beta.conversation.one/userpage'))
-.then( _ => driver.manage().window().maximize())
+.then( _ => z.openPage(baseUrl + '/userpage'))
+.then( _ => z.maximizeWindow() )
 .then( _ => z.clickByClassName('menu-item new-site'))
 .then( _ => z.assertExistsById('org-name', 'company name field') )  
 .catch( z.failedScenario )                               
 
 .then( _ => z.scenario('Clicking the next button should open the New app Ready screen') )
-.then( _ => z.openPage(' https://dashboard-beta.conversation.one/new'))
-.then( _ => driver.manage().window().maximize())
+.then( _ => z.openPage(baseUrl + '/new'))
+.then( _ => z.maximizeWindow() )
 .then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
 .then( _ => z.clickByLinkText('Custom')) 
 .then( _ => z.inputById ('org-name', 'test'))
@@ -40,7 +44,7 @@ driver
 //Scenario: Clicking the Sets button opens the rules sets section
 driver
 .then( _ => z.scenario('Clicking the Sets button opens the rules sets section') )
-.then( _ => driver.manage().window().maximize())
+.then( _ => z.maximizeWindow() )
 .then( _ => z. clickById ('btnShowEditInfo'))
 .then( _ => z.waitFor(3))
 .then( _ => z.assertExistsByCss('#rulesSetTableBody > tr:nth-child(1) > td.setName'), "rules sets section"  )
