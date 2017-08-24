93037373f6c3d237de9ebb9f99a177f94567f655
Unknown <larisayablonskaya@gmail.com>
2017-08-24 15:53:41 +0300

    changed urls

--- plain-selenium/log-in.js
+++ plain-selenium/log-in.js
@@ -2,10 +2,14 @@ const { By } = require('selenium-webdriver');
 const z = require('./common');
 const driver = z.getDriver();
 
+const config = require('config')
+const baseUrl = config.baseUrl
+const admin = config.creds.admin
+
 //Log In functionality testing with an admin user
 driver
 .then( _ => z.scenario('Log In without password should show an error message') )
-.then( _ => z.openPage('https://dashboard-beta.conversation.one/user/login', 'login page') )
+.then( _ => z.openPage(baseUrl + '/user/login', 'login page') )
 .then( _ => z.maximizeWindow() )
 .then( _ => z.inputById('edit-name','Tom@zuznow.com') )
 .then( _ => z.clickById('edit-submit') )
@@ -15,7 +19,7 @@ driver
 
 driver
 .then( _ => z.scenario('Log in with wrong password should show an error message'))
-.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
+.then( _ => z.openPage (baseUrl + '/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Tom@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Computer1'))
 .then( _ => z.clickById('edit-submit') )
@@ -25,7 +29,7 @@ driver
 
 driver
 .then( _ => z.scenario('Log in with wrong username should show an error message'))
-.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
+.then( _ => z.openPage (baseUrl + '/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Tim@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Adminuser1'))
 .then( _ => z.clickById('edit-submit') )
@@ -36,7 +40,7 @@ driver
 driver
 .then( _ => z.scenario('successful log in of admin user with applications should show homepage'))
 .then( _ => z.clearCookies() )
-.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
+.then( _ => z.openPage (baseUrl + '/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Tom@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Adminuser1'))
 .then( _ => z.clickById('edit-submit') )
@@ -46,7 +50,7 @@ driver
 driver
 .then( _ => z.scenario('successful log in of users without applications should show new-app screen'))
 .then( _ => z.clearCookies() )
-.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
+.then( _ => z.openPage (baseUrl + '/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Polly@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Newuser1'))
 .then( _ => z.clickById('edit-submit') )
@@ -56,7 +60,7 @@ driver
 driver
 .then( _ => z.scenario('successful log in of non-admin user with applications should show homepage'))
 .then( _ => z.clearCookies() )
-.then( _ => z.openPage ('https://dashboard-beta.conversation.one/user/login', 'login page') )
+.then( _ => z.openPage (baseUrl + '/user/login', 'login page') )
 .then( _ => z.inputById('edit-name','Martin@zuznow.com'))
 .then( _ => z.inputById('edit-pass', 'Newuser2'))
 .then( _ => z.clickById('edit-submit') )
