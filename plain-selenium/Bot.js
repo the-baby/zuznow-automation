dd267022ac67463fedbeb34cdf86cefb6936c480
Unknown <larisayablonskaya@gmail.com>
2017-09-11 16:24:29 +0300

    changes

--- plain-selenium/Bot.js
+++ plain-selenium/Bot.js
@@ -7,7 +7,7 @@ const z = require('./common');
 const scenario = z.scenario;
 const driver = z.getDriver();
 
-
+//Feature: Creating a new app with a custom industry
 
 scenario('Sign-in successfully leads to homepage')
 .then( _ => z.openPage(baseUrl + '/user/login', 'login page') )
@@ -20,30 +20,22 @@ scenario('Sign-in successfully leads to homepage')
 .catch( z.failedScenario )
 
 //Successful creation of a predefined banking app
-scenario('Clicking the Bot icon marks it')
+scenario('Clicking the Next button opens the Test your skill screen')
 .then( _ => z.openPage(baseUrl + '/new'))
 .then( _ => z.maximizeWindow() ) 
 .then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
 .then( _ => z.clickByLinkText('Banking'))
 .then( _ => z.inputById ('org-name', 'My'))
-.then( _ => z.waitFor(3))
 .then( _ => z. clickByCss ('#tab1 > div.form-horizontal > div:nth-child(2) > div.col-md-6 > div > ul > li.active > a'))
 .then( _ => z.clickByClassName ('pager wizard'))
 .then( _ => z.waitFor(2))
-.then(_ => z.clickByCss('#tab3 > div.form-horizontal > div > div:nth-child(6) > label.selection-btn-primary > img'))
-//.then( _ => z.assertElementHasClass(By.css('#tab3 > div.form-horizontal > div > div:nth-child(6) > label.selection-btn-primary > img'), "the checkbox is marked", "checkmark draw"))
 .then( _ => z. clickById ('btnNext'))
-.then( _ => z.assertExistsByClassName('fa fa-play-circle fa-6', 'the Play simulator button' ))
+.then( _ => z.assertExistsByClassName('fa fa-play-circle fa-6'),'the Play simulator button' )
 .catch( z.failedScenario )
 
 scenario('Clicking the Customize button opens the Interaction Editor tab')
-.then( _ => z.clickByXPath('//*[@id="btnFinish"]/span'))
-.then( _ => z.waitFor(60))
+.then( _ => z.clickByXPath ('//*[@id="btnFinish"]/span'))
+.then( _ => z.locate(By.id('btnSave')))
+.then( _ => z.waitFor(2))
+.then( _ => z.assertExistsById('accordion_interaction'), "the Interaction tab is open")
 .catch( z.failedScenario )
-
-//Feature: Chat bot button and screen
-scenario('The corresponding button opens the chat bot menu')
-.then( _ => z.clickByCss('#toggle_simulator'))                           
-.then( _ => z.switchTab(1, 'popped up conversation window'))  
-.then( _ => z.locate(By.css('#conv_input > div'), 'chat frame'))
-.then( _ => z.assertExistsByCss('#conv_input > div > span:nth-child(2) > button', 'the clear history button'))                
