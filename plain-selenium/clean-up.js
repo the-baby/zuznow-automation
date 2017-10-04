b048d15dd96f057bcad20f6177e912e6196deaec
Unknown <larisayablonskaya@gmail.com>
2017-10-04 13:00:00 +0300

    fixed according to the changes

--- plain-selenium/clean-up.js
+++ plain-selenium/clean-up.js
@@ -28,11 +28,4 @@ driver
 .then( _ => driver.switchTo().alert().accept() )
 .catch( z.failedScenario )
 
-driver
-.then( _ => z.scenario('Clicking the Users button opens the users tab') )
-.then( _ => z.clickByCss('#main-menu > li.menu-230 > a') )
-.then( _ => z.clickById('user_selection') )
-.then( _ => z.clickByCss('#user_selection > option:nth-child(5)') )
-.then( _ => z.clickById('btnDeleteUser') ) 
-.then( _ => driver.switchTo().alert().accept() )
-.catch( z.failedScenario )
+
