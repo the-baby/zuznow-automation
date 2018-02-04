adb55b863e54146d736a63e18ff3ee4c889b7b56
Unknown <larisayablonskaya@gmail.com>
2018-02-04 15:30:58 +0200

    changes

--- plain-selenium/configuration.js
+++ plain-selenium/configuration.js
@@ -32,23 +32,32 @@ driver
 .then( _ => z.scenario('Clicking the next button should open the New app Ready screen') )
 .then( _ => z.openPage(' https://dashboard-beta.conversation.one/new'))
 .then( _ => z.maximizeWindow() )
-.then( _ => z.clickByClassName('btn btn-default dropdown-toggle')) 
-.then( _ => z.clickByLinkText('Education')) 
-.then( _ => z.inputById ('org-name', 'test'))
-.then( _ => z.clickByClassName ('btn btn-success btn-square next button-next full-width'))
+.then( _ => z.clickById('s2id_industry_select'))
+.then( _ => z.inputByCss('.select2-input','Education'))
+.then( _ => z.clickByCss('li.select2-result.select2-highlighted'))
+ .then( _ => z.clickById('btnNext')) 
+.then( _ => z.inputById('org-name', 'test'))
+.then( _ => z.clickByClassName('btn  button-next'))
 .then( _ => z.waitFor(2))
-.then( _ => z. clickById ('btnNext'))
+.then( _ => z.inputByCss('input.form-control.newIntentSample.first', 'how can I contact support'))
 .then( _ => z.waitFor(4))
-.then( _ => z.clickById('btnFinish'))
+.then( _ => z.clickById ('btnNext'))
+.then( _ => z.inputByCss ('input.form-control.newTextResponse.first', 'you can call us at 1800'))
+.then( _ => z.clickById ('btnNext'))
+.then( _ => z.waitFor(8))
+//.then( _ => switchTo().frame('chatbot_frame'))
+//.then( _ => z.inputById('input', 'how can I contact support'))
+//.then( _ => z.clickByClassName ('submitBtn form-control c1Icon c1Icon-paper-plane'))	
+.then( _ => z.clickByClassName('publish-btn btn editor'))
 
 
 driver
-.then( _ => z.scenario(' Clicking the configuration button opens the general part of the Configuration tab') )
+.then( _ => z.scenario(' Clicking the configuration button opens the Production part of the Configuration tab') )
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.waitFor(2))
 //.then( _ => driver.switchTo().alert().accept() ) 
 .then( _ => z.waitFor(2))
-.then( _ => z.assertExistsByCss('#tab-1 > div.col-lg-8 > div:nth-child(1) > div.portlet-header > div'), 'the general tab')
+.then( _ => z.assertExistsById('name'), 'the production tab')
 .catch( z.failedScenario )
 
 
@@ -60,7 +69,7 @@ driver
 .then( _ => z.waitFor(2))
 .then( _ => z.clickByLinkText('Staging'))
 //.then( _ => driver.switchTo().alert().accept() ) 
-.then( _ => z.assertExistsByCss('#settings-panel-stg > div.portlet-header > div'), 'the staging tab opened')
+.then( _ => z.assertExistsById('name_stg'), 'the staging tab opened')
 .catch( z.failedScenario )
 
 //Selecting a certain value from the Log level drop-down menu
@@ -71,14 +80,14 @@ driver
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.waitFor(2))
 .then( _ => z.clickByLinkText('Staging'))
-.then( _ => z. clickById('log_level_stg'))
-.then( _ => z. clickByCss('#log_level_stg > option:nth-child(3)'))
+.then( _ => z. clickById('s2id_log_level_stg'))
+.then( _ => z. clickByCss('.select2-result:first-child'))
 .then( _ => z. clickById('btnSave'))
 .then( _ => z.waitFor(4))
 .then( _ => z. clickByCss('#btnEdit > i'))
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.clickByLinkText('Staging'))
-.then( () => z.assertContainsValue(By.css('#log_level_stg'), "the expected text in the element", 'warn') )
+.then( () => z.assertContainsValue(By.css('#log_level_stg'), "the expected text in the element", 'fatal') )
 .catch(z.failedScenario)
 
 
@@ -91,10 +100,11 @@ driver
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.waitFor(2))
 .then( _ => z.clickByLinkText('Staging'))
-.then( _ => z.clickByLinkText('Alexa'))
+.then( _ => z.clickById('memu_btn_alexa_stg'))
 .then( _ => z.assertExistsByCss('#alexa_support_stg:checked'), 'the checkbox is marked')
 .then( _ => z.assertExistsByCss('#alexa_account_linking_stg:checked'), 'the checkbox is marked')
 .catch(z.failedScenario)
+
 
 //Clicking the Generate button generates client id and client secret values
 
@@ -103,11 +113,12 @@ driver
 .then( _ => z.maximizeWindow() )
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.clickByLinkText('Staging'))
-.then( _ => z.clickByLinkText('Alexa'))
+.then( _ => z.clickById('memu_btn_alexa_stg'))
 .then( _ => z.inputById('alexa_redirect_urls_stg', 'https://layla.amazon.com/api/skill/link/M1QDRNZCTF1O3Q/nhttps://pitangui.amazon.com/api/skill/link/M1QDRNZCTF1O3Q'))
 .then( _ => z.scrollToBottom())
 .then( _ => z.clickById('btnGenerateAlexaLinking_stg', 'Generate button')  )
-//.then( _ => z.assertContainsValue(By.id('alexa_client_id_stg'), "the expected text in the element", 'alexa'))
+.then( _ => z.waitFor(2))
+.then( _ => z.assertContainsValue(By.id('alexa_client_id_stg'), "the expected text in the element", 'alexa_'))
 //.then( _ => z.assertContainsValue(By.id('alexa_client_secret_stg'), "the expected text in the element", 'lM01me7zL6'))
 .catch(z.failedScenario)
 
@@ -120,20 +131,13 @@ driver
 .then( _ => z.maximizeWindow() )
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.clickByLinkText('Staging'))
-.then( _ => z.clickByLinkText('Google'))
+.then( _ => z.clickById('memu_btn_google_stg'))
 .then( _ => z.assertExistsByCss('#google_support_stg:checked'), 'the checkbox is marked')
 .then( _ => z.assertExistsByCss('#google_account_linking_stg:checked'), 'the checkbox is marked')
 .catch(z.failedScenario)
 
-//Feature: Production tab
-//Clicking the production button opens the Production tab
 
-.then( _ => z.scenario('Clicking the Production button opens the productoin tab') )
-.then( _ => z.openPage(baseUrl + '/editor'))
-.then( _ => z.clickById('btnMyApps', 'configuration button')  )
-.then( _ => z.clickByLinkText('Production'))
-.then( _ => z.assertExistsByCss('#prod-tab > li.active > a'), 'the production tab opened')
-.catch( z.failedScenario )
+//Feature: Production tab
 
 //Selecting a certain value from the Log level drop-down menu
 
@@ -141,13 +145,15 @@ driver
 .then( _ => z.openPage(baseUrl + '/editor'))
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.clickByLinkText('Production'))
-.then( _ => z. clickById ('log_level'))
-.then( _ => z. clickByCss ('#log_level > option:nth-child(5)'))
+.then( _ => z. clickById('s2id_log_level'))
+.then( _ => z. clickByCss('.select2-result:first-child'))
+.then( _ => z.waitFor(1))
 .then( _ => z. clickById('btnSave'))
+.then( _ => z.waitFor(1))
 .then( _ => z. clickByCss ('#btnEdit > i'))
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.clickByLinkText('Production'))
-.then( () => z.assertContainsValue(By.css('#log_level'), "the expected text in the element", 'debug') )
+.then( () => z.assertContainsValue(By.css('#log_level'), "the expected text in the element", 'fatal') )
 .catch(z.failedScenario)
 
 //Feature: Alexa subtab (Production)
@@ -158,7 +164,7 @@ driver
 .then( _ => z.maximizeWindow() )
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.clickByLinkText('Production'))
-.then( _ => z.clickByLinkText('Alexa'))
+.then( _ => z.clickById('menu_btn_alexa'))
 .then( _ => z.assertExistsByCss('#alexa_support:checked'), 'the checkbox is marked')
 .then( _ => z.assertExistsByCss('#alexa_account_linking:checked'), 'the checkbox is marked')
 .catch(z.failedScenario)
@@ -169,11 +175,12 @@ driver
 .then( _ => z.maximizeWindow() )
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.clickByLinkText('Production'))
-.then( _ => z.clickByLinkText('Alexa'))
+.then( _ => z.clickById('menu_btn_alexa'))
 .then( _ => z.inputById('alexa_redirect_urls', 'https://layla.amazon.com/api/skill/link/M1QDRNZCTF1O3Q/nhttps://pitangui.amazon.com/api/skill/link/M1QDRNZCTF1O3Q'))
 .then( _ => z.scrollToBottom())
 .then( _ => z.clickById('btnGenerateAlexaLinking', 'Generate button')  )
-//.then( _ => z.assertContainsValue(By.id('alexa_client_id'), "the expected text in the element", 'alexa'))
+.then( _ => z.waitFor(3))
+.then( _ => z.assertContainsValue(By.id('alexa_client_id'), "the expected text in the element", 'alexa'))
 //.then( _ => z.assertContainsValue(By.id('alexa_client_secret'), "the expected text in the element", 'lM01me7zL6'))
 .catch(z.failedScenario)
 
@@ -185,10 +192,11 @@ driver
 .then( _ => z.openPage(baseUrl + '/editor'))
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
 .then( _ => z.clickByLinkText('Production'))
-.then( _ => z.clickByLinkText('Google'))
+.then( _ => z.clickById('menu_btn_google'))
 .then( _ => z.assertExistsByCss('#google_support:checked'), 'the checkbox is marked')
 .then( _ => z.assertExistsByCss('#google_account_linking:checked'), 'the checkbox is marked')
 .catch(z.failedScenario)
+
 
 //Feature: Delete skill
 //Clicking Delete button deletes the app
@@ -197,10 +205,10 @@ driver
 .then( _ => z.openPage(baseUrl + '/editor'))
 .then( _ => z.maximizeWindow() )
 .then( _ => z.clickById('btnMyApps', 'configuration button')  )
-.then( _ => z.clickByCss ('#btnDelete > i.fa.fa-trash-o') )
-.then( _ => z.waitFor(3))
+.then( _ => z.clickById('btnDelete') )
 .then( _ => driver.switchTo().alert().accept() ) 
-//.then( _ => z.assertExistsByCss('#domains_table > div.panel-heading > span'), 'the user is on My Apps screen') - needs to be changed
+.then( _ => z.waitFor(5))
+//.then( _ => z.assertNoSuchElements(By.css('menu_btn_settings:visible'), 'settings' ) )
 .catch(z.failedScenario)
 
 driver
