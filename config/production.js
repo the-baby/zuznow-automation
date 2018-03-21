33addd87b24c3c35b3b1a58c0131a9e314c64288
Unknown <larisayablonskaya@gmail.com>
2018-03-21 14:46:04 +0200

    Created a new folder for production

--- config/production.js
+++ config/production.js
@@ -1,9 +1,9 @@
 module.exports = {
   baseUrl: 'https://dashboard.conversation.one',
   creds: {
-    admin: {
-      user:         'admin',
-      password:     'vs8Sr7aU'
+    nonadmin: {
+      user:         'AutomationTest',
+      password:     'Automation123'
     }
   }
 }
